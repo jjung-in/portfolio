0f967286b2e306a29430a3c53a0ef235ccb2508e
jjungiin <4292in@naver.com>
2022-05-09 03:41:17 +0900

    Initial Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./reset.scss";
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Main from "./pages/Main";
+// import Product from "./Product";
+// import NotFound from './NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        {/* <Route path="/product/:id" element={<Product />} /> */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.scss";
+
+function Header() {
+	return (
+		<div>
+			헤더
+		</div>
+	);
+}
+
+export default Header;
