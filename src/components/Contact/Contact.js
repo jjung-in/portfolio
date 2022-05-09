9af64df31e7e63ed8f3008328478f1e6e4f0b222
jjungiin <4292in@naver.com>
2022-05-10 03:15:29 +0900

    contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Contact.scss";
 
 function Contact() {
@@ -6,7 +7,35 @@ function Contact() {
     <div id="contact">
       <div className="container contact_wrap">
         <h2>CONTACT</h2>
-        <div className="info_wrap"></div>
+        <div className="info_wrap">
+          <div className="text_wrap">
+            <h6>E-MAIL</h6>
+            <p>4292in@naver.com</p>
+          </div>
+          <div className="text_wrap">
+            <h6>CALL</h6>
+            <p>010-5069-4292</p>
+          </div>
+          <div className="text_wrap">
+            <h6>GITHUB</h6>
+            <p>
+              jung-in{" "}
+              <a href="https://github.com/jjung-in" target="_blank">
+                GITHUB
+              </a>
+            </p>
+          </div>
+          <div className="text_wrap">
+            <h6>RESUME</h6>
+            <p>
+              jung-in{" "}
+              <a href="/" target="_blank">
+                RESUME
+              </a>
+            </p>
+          </div>
+          <footer id="footer">Copyright 2022. Jungin Go All rights reserved.</footer>
+        </div>
       </div>
     </div>
   );
