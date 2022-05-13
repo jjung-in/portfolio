52a6f6c47c4e978e336d5d6937e716ca64994f60
jjungiin <4292in@naver.com>
2022-05-13 16:39:51 +0900

    more

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,33 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 function Header() {
+  const [isUrl, setIsUrl] = useState(true);
+  console.log(isUrl);
+
+  useEffect(() => {
+    if (window.location.pathname === "/portfolio/project") {
+      setIsUrl(false);
+    }
+  }, []);
+
   return (
     <header id="header">
       <div className="container header_inwrap">
-        <h1><Link to="/">GO JUNG-IN</Link></h1>
-        <nav id="nav">
-          <ul>
-            <li>HOME</li>
-            <li>ABOUT</li>
-            <li>PROJECT</li>
-            <li>CONTACT</li>
-          </ul>
-        </nav>
+        <h1>
+          <Link to="/">GO JUNG-IN</Link>
+        </h1>
+        {isUrl && (
+          <nav id="nav">
+            <ul>
+              <li>HOME</li>
+              <li>ABOUT</li>
+              <li>PROJECT</li>
+              <li>CONTACT</li>
+            </ul>
+          </nav>
+        )}
       </div>
     </header>
   );
--- src/pages/More.js
+++ src/pages/More.js
@@ -1,11 +1,108 @@
-import React from "react";
-// import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./More.scss";
+import portfolio_preview from "../assets/img_portfolio/portfolio_preview.png";
+import oramyun_preview from "../assets/img_oramyun/oramyun_preview.png";
+import doran_preview from "../assets/img_doran/doran_preview.png";
+import chic_preview from "../assets/img_chic/chic_preview.png";
+import paris_preview from "../assets/img_paris/paris_preview.png";
+import crain_preview from "../assets/img_crain/crain_preview.png";
 
 function More() {
+  const [ScrollY, setScrollY] = useState(0);
+  const [BtnStatus, setBtnStatus] = useState(false); // 버튼 상태
+
+  const handleFollow = () => {
+    setScrollY(window.pageYOffset);
+    if (ScrollY > 100) {
+      // 100 이상이면 버튼이 보이게
+      setBtnStatus(true);
+    } else {
+      // 100 이하면 버튼이 사라지게
+      setBtnStatus(false);
+    }
+  };
+
+  const handleTop = () => {
+    // 클릭하면 스크롤이 위로 올라가는 함수
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    setScrollY(0); // ScrollY 의 값을 초기화
+    setBtnStatus(false); // BtnStatus의 값을 false로 바꿈 => 버튼 숨김
+  };
+
+  useEffect(() => {
+    const watch = () => {
+      window.addEventListener("scroll", handleFollow);
+    };
+    watch();
+    return () => {
+      window.removeEventListener("scroll", handleFollow);
+    };
+  });
+
   return (
     <div id="more">
-      <div className="container">프로젝트 더보기 페이지</div>
+      <div className="container">
+        <h2>PROJECT</h2>
+        <button
+          className={BtnStatus ? "topBtn active" : "topBtn"} // 버튼 노출 여부
+          onClick={handleTop} // 버튼 클릭시 함수 호출
+        ></button>
+        <ul>
+          <li>
+            <img src={portfolio_preview} alt="portfolio" />
+            <div className="text_wrap">
+              <Link to="/project/portfolio">
+                <span>포트폴리오</span>
+              </Link>
+            </div>
+          </li>
+          <li>
+            <img src={oramyun_preview} alt="oramyun" />
+            <div className="text_wrap">
+              <Link to="/project/oramyun">
+                <span>오늘의 라면</span>
+              </Link>
+            </div>
+          </li>
+          <li>
+            <img src={doran_preview} alt="doran-doran" />
+            <div className="text_wrap">
+              <Link to="/project/doran">
+                <span>도란도란</span>
+              </Link>
+            </div>
+          </li>
+          <li>
+            <img src={chic_preview} alt="chic-chic" />
+            <div className="text_wrap">
+              <Link to="/project/chic">
+                <span>칰칰폭폭</span>
+              </Link>
+            </div>
+          </li>
+          <li>
+            <img src={paris_preview} alt="paris" />
+            <div className="text_wrap">
+              <Link to="/project/paris">
+                <span>파리바게트</span>
+              </Link>
+            </div>
+          </li>
+          <li>
+            <img src={crain_preview} alt="crain" />
+            <div className="text_wrap">
+              <Link to="/project/crain">
+                <span>크레인 고장진단 시스템</span>
+              </Link>
+            </div>
+          </li>
+          
+        </ul>
+      </div>
     </div>
   );
 }
